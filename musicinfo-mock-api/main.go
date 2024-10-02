2e2654e1a87f1af43e863b80acda912e3f5604e0
Misha Karpov <mishavkrpv@gmail.com>
2024-10-02 11:55:50 +0300

    Add musicinfo mock api

---
+++ musicinfo-mock-api/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/info", func(c *gin.Context) {
+		group := c.Query("group")
+		song := c.Query("song")
+
+		if group == "" || song == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "query params should contain both group and song"})
+			return
+		}
+
+		songDetail := &SongDetail{
+			ReleaseDate: time.Now().Format("2006.01.02"),
+			Text:        "Ooh baby, don't you know I suffer?\nOoh baby, can you hear me moan?\nYou caught me under false pretenses\nHow long before you let me go?\n\nOoh\nYou set my soul alight\nOoh\nYou set my soul alight",
+			Link:        "https://www.youtube.com/watch?v=Xsp3_a-PMTw",
+		}
+
+		c.JSON(http.StatusOK, &songDetail)
+	})
+
+	r.Run(":3256")
+}
+
+type SongDetail struct {
+	ReleaseDate string
+	Text        string
+	Link        string
+}
