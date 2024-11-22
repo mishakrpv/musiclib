e5ba9540a84a41e1b951e8067f2c953690481828
mishakrpv <mishavkrpv@gmail.com>
2024-11-22 22:42:06 +0300

    feat: replace gin logger

--- internal/router/router.go
+++ internal/router/router.go
@@ -6,6 +6,9 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/mishakrpv/musiclib/internal/domain/song"
 	"github.com/mishakrpv/musiclib/pkg/infra/musicinfo"
+	"github.com/mishakrpv/musiclib/pkg/logger"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 type Router struct {
@@ -16,8 +19,12 @@ type Router struct {
 }
 
 func New(client musicinfo.Client, songRepo song.Repository) http.Handler {
+	engine := gin.New()
+
+	engine.Use(gin.LoggerWithWriter(logger.NoLevel(log.Logger, zerolog.InfoLevel)))
+
 	router := &Router{
-		engine:          gin.New(),
+		engine:          engine,
 		musicinfoClient: client,
 		songRepo:        songRepo,
 	}
